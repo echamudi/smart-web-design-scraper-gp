ef94424f9d4fd5eedcdb5ba77b21fe4f0ecd5208
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-12 00:22:47 +0300

    Update home and analysis result

--- client/src/app/analysis-result/analysis-result.component.ts
+++ client/src/app/analysis-result/analysis-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-analysis-result',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalysisResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  url: string;
+  size: string;
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.url = params['url'];
+      this.size = params['size'];
+    });
   }
-
 }

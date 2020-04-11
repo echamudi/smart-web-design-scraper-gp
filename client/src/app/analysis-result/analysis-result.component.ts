ce3e4f6e2741806b564719f7788fac63381eb6ed
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-12 02:52:13 +0300

    Update analysis result page

--- client/src/app/analysis-result/analysis-result.component.ts
+++ client/src/app/analysis-result/analysis-result.component.ts
@@ -13,15 +13,28 @@ export class AnalysisResultComponent implements OnInit {
 
   url: string;
   size: string;
+  imageURL: string;
+  resultHtmlURL: string;
+  analysisDescription: string;
+  showResult: boolean;
 
   ngOnInit(): void {
+    this.showResult = false;
+
     this.activatedRoute.queryParams.subscribe(params => {
       this.url = params['url'];
       this.size = params['size'];
 
       this.http.get('http://localhost:3302/api/analyze?url=' + encodeURI(this.url) + '&size=' + encodeURI(this.size))
-        .subscribe(data => console.log(data));
+        .subscribe((data: any) => {
+          console.log(data);
 
+          this.imageURL = 'http://localhost:3302/' + data.resultScreenshotURL;
+          this.resultHtmlURL = 'http://localhost:3302/' + data.resultHtmlURL;
+          this.analysisDescription = data.analysisDescription;
+
+          this.showResult = true;
+        });
     });
   }
 }

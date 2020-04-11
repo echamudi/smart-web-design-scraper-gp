534d2304a1d0aa60a50265ec06b0640ce3bb91c8
Ayman Rahmon <ayman_rahmon@hotmail.com>
2020-04-11 15:44:22 +0300

    initial backend set up

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express'); 
+const bodyParser = require('body-parser') ; 
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose'); 
+
+
+const dbConfig = require('./config/dbconf.js');
+mongoose.Promise = global.Promise ; 
+mongoose
+  .connect(
+    dbConfig.url,
+    { useNewUrlParser: true}
+  )
+  .then(
+    () => {
+      console.log('Connected to mongoDB');
+    },
+    (err) => console.log('Error connecting to mongoDB', err)
+  );
+
+const app = express() ; 
+const port = process.env.PORT || 3000 ; 
+
+// sets up the middleware for parsing bodies and cookies off of the request.
+app.use(bodyParser.json());
+app.use(cookieParser()); 
+
+
+app.listen(port,()=>{
+    console.log('server running on port  ' + port);
+}) ;
+
+
+module.exports = {app};
+
+
+

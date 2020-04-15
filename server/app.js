d940461fff9b51e57751eb81c9d944270ed80174
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-15 13:01:19 +0300

    Add eslint for server

--- server/app.js
+++ server/app.js
@@ -3,38 +3,34 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const dbConfig = require('./config/dbconf.js');
+
 mongoose.Promise = global.Promise;
 mongoose
   .connect(
     dbConfig.url,
-    { useNewUrlParser: true }
+    { useNewUrlParser: true },
   )
   .then(
     () => {
       console.log('Connected to mongoDB');
     },
-    (err) => console.log('Error connecting to mongoDB', err)
+    (err) => console.log('Error connecting to mongoDB', err),
   );
 const app = express();
 const port = 3302;
-
 
 // sets up the middleware for parsing bodies and cookies off of the request.
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// routes ... 
+// routes ...
 const userRoute = require('./routes/users');
+
 app.use('/api/users', userRoute);
 
-
-
 app.listen(port, () => {
-  console.log('server running on port  ' + port);
+  console.log(`server running on port  ${port}`);
 });
 
 
 module.exports = { app };
-
-
-

dfca4c9e3fcc519ba90fc613023980379197fb2b
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-15 12:33:05 +0300

    Change port number

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-const express = require('express'); 
-const bodyParser = require('body-parser') ; 
+const express = require('express');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const dbConfig = require('./config/dbconf.js');
-mongoose.Promise = global.Promise ; 
+mongoose.Promise = global.Promise;
 mongoose
   .connect(
     dbConfig.url,
-    { useNewUrlParser: true}
+    { useNewUrlParser: true }
   )
   .then(
     () => {
@@ -15,26 +15,26 @@ mongoose
     },
     (err) => console.log('Error connecting to mongoDB', err)
   );
-  const app = express() ; 
-  const port = process.env.PORT || 3000 ; 
-  
+const app = express();
+const port = 3301;
+
 
 // sets up the middleware for parsing bodies and cookies off of the request.
 app.use(bodyParser.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 // routes ... 
-const userRoute  = require('./routes/users');
-app.use('/api/users',userRoute);
+const userRoute = require('./routes/users');
+app.use('/api/users', userRoute);
 
 
 
-app.listen(port,()=>{
-    console.log('server running on port  ' + port);
-}) ;
+app.listen(port, () => {
+  console.log('server running on port  ' + port);
+});
 
 
-module.exports = {app};
+module.exports = { app };
 
 
 

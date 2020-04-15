860796a9990dd4a35e30feb352e63a7d0d9a2a0b
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-15 21:28:40 +0300

    Update server test

--- test/server-api.js
+++ test/server-api.js
@@ -23,6 +23,7 @@ describe('Server API test', () => {
                 username: `testuser${ts}`,
                 email: `test${ts}@test.com`,
                 password: '123456789',
+                roles: ['user'],
             })
             .set('Content-Type', 'application/json')
             .set('Accept', 'application/json')

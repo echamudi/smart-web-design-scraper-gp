f9470af680e1e461f35b9c77f68b0a9b777464af
Ezzat Chamudi <chamudi.git@outlook.com>
2020-04-20 05:50:39 +0300

    Add vibrant checker

--- server/routes/analyzer.routes.js
+++ server/routes/analyzer.routes.js
@@ -30,7 +30,7 @@ module.exports = function (app) {
 
                 const response = await page.goto(url);
 
-                if (response._statusText === 'OK') {
+                if (response._status === 200) {
                     pageResponseWell = true;
                 } else {
                     return;
@@ -43,6 +43,8 @@ module.exports = function (app) {
                 const inputData = {
                     fontSizeSmallLimit,
                 };
+                fs.mkdirSync('./temp', { recursive: true });
+                await page.screenshot({ path: './temp/result-1-vanilla.png' });
 
                 const data = await page.evaluate(async (inputData) => {
                     let smallTextCount = 0;
@@ -64,32 +66,32 @@ module.exports = function (app) {
                     }
 
                     // Get Style
-                    const css = [];
-                    for (let i = 0; i < document.styleSheets.length; i += 1) {
-                        const sheet = document.styleSheets[i];
-                        const rules = ('cssRules' in sheet) ? sheet.cssRules : sheet.rules;
-                        if (rules) {
-                            css.push(`\n/* Stylesheet : ${sheet.href || '[inline styles]'} */`);
-                            for (let j = 0; j < rules.length; j += 1) {
-                                const rule = rules[j];
-                                if ('cssText' in rule) css.push(rule.cssText);
-                                else css.push(`${rule.selectorText} {\n${rule.style.cssText}\n}\n`);
-                            }
-                        }
-                    }
-                    const cssString = `${css.join('\n')}\n`;
+                    // const css = [];
+                    // for (let i = 0; i < document.styleSheets.length; i += 1) {
+                    //     const sheet = document.styleSheets[i];
+                    //     const rules = ('cssRules' in sheet) ? sheet.cssRules : sheet.rules;
+                    //     if (rules) {
+                    //         css.push(`\n/* Stylesheet : ${sheet.href || '[inline styles]'} */`);
+                    //         for (let j = 0; j < rules.length; j += 1) {
+                    //             const rule = rules[j];
+                    //             if ('cssText' in rule) css.push(rule.cssText);
+                    //             else css.push(`${rule.selectorText} {\n${rule.style.cssText}\n}\n`);
+                    //         }
+                    //     }
+                    // }
+                    // const cssString = `${css.join('\n')}\n`;
 
                     // Remove CSS linking from HTML
-                    const hs = document.querySelectorAll('link[rel="stylesheet"]');
-                    for (let i = 0, max = hs.length; i < max; i += 1) {
-                        hs[i].parentNode.removeChild(hs[i]);
-                    }
+                    // const hs = document.querySelectorAll('link[rel="stylesheet"]');
+                    // for (let i = 0, max = hs.length; i < max; i += 1) {
+                    //     hs[i].parentNode.removeChild(hs[i]);
+                    // }
 
                     // Add style to head
-                    const head = document.head || document.getElementsByTagName('head')[0];
-                    const style = document.createElement('style');
-                    head.appendChild(style);
-                    style.appendChild(document.createTextNode(cssString));
+                    // const head = document.head || document.getElementsByTagName('head')[0];
+                    // const style = document.createElement('style');
+                    // head.appendChild(style);
+                    // style.appendChild(document.createTextNode(cssString));
 
                     // Get Entire HTML
                     const htmlString = document.documentElement.innerHTML;
@@ -100,9 +102,8 @@ module.exports = function (app) {
                     };
                 }, inputData);
 
-                fs.mkdirSync('./temp', { recursive: true });
                 fs.writeFileSync('./temp/result-1.html', data.html);
-                await page.screenshot({ path: './temp/result-1.png' });
+                await page.screenshot({ path: './temp/result-1-font-size.png' });
 
                 factorData.smallTextCount = data.smallTextCount;
 
@@ -122,7 +123,7 @@ module.exports = function (app) {
                 inputSize: size,
                 factorData,
                 resultHtmlURL: 'result-1.html',
-                resultScreenshotURL: 'result-1.png',
+                resultScreenshotURL: 'result-1',
                 analysisDescription: desc,
             });
         } else {

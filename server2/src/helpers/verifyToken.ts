f460918afc5e2da797528b0f9eef8049a7b2bbad
Ezzat Chamudi <echamudi@outlook.com>
2020-09-26 12:19:30 +0300

    Add comment and update signature

--- server2/src/helpers/verifyToken.ts
+++ server2/src/helpers/verifyToken.ts
@@ -1,7 +1,11 @@
 import jwt, { JsonWebTokenError, NotBeforeError, TokenExpiredError } from 'jsonwebtoken';
 import config from '../configs/auth.config';
 
-export const verifyToken = (token: string): Promise<{id: string} | JsonWebTokenError | NotBeforeError | TokenExpiredError | null>  => {
+/**
+ * Check whether jwt token is valid or not, it returns the id property of the payload
+ * @param token JWT token
+ */
+export const verifyToken = (token: string): Promise<string | JsonWebTokenError | NotBeforeError | TokenExpiredError | null>  => {
     if (typeof token !== 'string') {
         return Promise.resolve(null);
     }
@@ -11,7 +15,7 @@ export const verifyToken = (token: string): Promise<{id: string} | JsonWebTokenE
             if (err || decoded === undefined) {
                 resolve(err);
             }
-            resolve({id: (decoded as any).id});
+            resolve(((decoded as any).id as string));
         });
     });
 };

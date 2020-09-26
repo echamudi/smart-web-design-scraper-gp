14c9561bb6d81272fcf827c64b4550909fe9a4bf
Ezzat Chamudi <echamudi@outlook.com>
2020-09-26 04:29:21 +0300

    Add login logic

--- server2/src/resolvers.ts
+++ server2/src/resolvers.ts
@@ -1,3 +1,57 @@
+import { IResolverObject, IResolvers, IFieldResolver } from 'apollo-server';
+import jwt, { VerifyErrors } from 'jsonwebtoken';
+import config from './configs/auth.config';
+import bcrypt from 'bcryptjs';
+
+import { db } from './models';
+
+const User = db.user;
+const Role = db.role;
+
 // TODO: write resolvers
-export const resolvers = {
+
+export const resolvers: IResolvers = {
+    Query: {
+        login: async (parent, args, context, info) => {
+            const user = await User.findOne({
+                username: args.username,
+            }).populate('roles', '-__v');
+
+            if (!user) {
+                return {
+                    message: 'User not found!'
+                };
+            }
+
+            const passwordIsValid = bcrypt.compareSync(
+                args.password,
+                user.password,
+            );
+
+            if (!passwordIsValid) {
+                return {
+                    message: 'Invalid Password!'
+                };
+            }
+
+            const token = jwt.sign({ id: user.id }, config.secret, {
+                expiresIn: 86400, // 24 hours
+            });
+
+            const authorities = [];
+
+            for (let i = 0; i < user.roles.length; i += 1) {
+                authorities.push(`ROLE_${user.roles[i].name.toUpperCase()}`);
+            }
+
+            return {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    roles: user.roles.map(el => el.name),
+                },
+                token: token
+            };
+        }
+    },
 };
--- server2/src/typeDefs.ts
+++ server2/src/typeDefs.ts
@@ -2,4 +2,28 @@ import { gql } from "apollo-server-express";
 
 // TODO: write schema
 export const typeDefs = gql`
+    type Role {
+        name: String
+    }
+
+    type User {
+        username: String!
+        email: String!
+        roles: [String!]!
+    }
+
+    type Mutation {
+        signup(username: String!, password: String!, email: String!): User
+    }
+
+    type LoginResult {
+        message: String
+        user: User
+        token: String
+    }
+
+    type Query {
+        getCurrentUser(token: String): User
+        login(username: String!, password: String!): LoginResult
+    }
 `;

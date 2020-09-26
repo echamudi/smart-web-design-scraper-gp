e8ac3afd69154387272ebf5b5fa67ba47cbe32bc
Ezzat Chamudi <echamudi@outlook.com>
2020-09-26 04:41:24 +0300

    Update resolvers

--- server2/src/resolvers.ts
+++ server2/src/resolvers.ts
@@ -8,50 +8,51 @@ import { db } from './models';
 const User = db.user;
 const Role = db.role;
 
-// TODO: write resolvers
+const login: IFieldResolver<any, any> = async (parent, args, context, info) => {
+    const user = await User.findOne({
+        username: args.username,
+    }).populate('roles', '-__v');
+
+    if (!user) {
+        return {
+            message: 'User not found!'
+        };
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+        args.password,
+        user.password,
+    );
+
+    if (!passwordIsValid) {
+        return {
+            message: 'Invalid Password!'
+        };
+    }
+
+    const token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400, // 24 hours
+    });
+
+    const authorities = [];
+
+    for (let i = 0; i < user.roles.length; i += 1) {
+        authorities.push(`ROLE_${user.roles[i].name.toUpperCase()}`);
+    }
+
+    return {
+        user: {
+            username: user.username,
+            email: user.email,
+            roles: user.roles.map(el => el.name),
+        },
+        token: token
+    };
+};
 
 export const resolvers: IResolvers = {
     Query: {
-        login: async (parent, args, context, info) => {
-            const user = await User.findOne({
-                username: args.username,
-            }).populate('roles', '-__v');
-
-            if (!user) {
-                return {
-                    message: 'User not found!'
-                };
-            }
-
-            const passwordIsValid = bcrypt.compareSync(
-                args.password,
-                user.password,
-            );
-
-            if (!passwordIsValid) {
-                return {
-                    message: 'Invalid Password!'
-                };
-            }
-
-            const token = jwt.sign({ id: user.id }, config.secret, {
-                expiresIn: 86400, // 24 hours
-            });
-
-            const authorities = [];
-
-            for (let i = 0; i < user.roles.length; i += 1) {
-                authorities.push(`ROLE_${user.roles[i].name.toUpperCase()}`);
-            }
-
-            return {
-                user: {
-                    username: user.username,
-                    email: user.email,
-                    roles: user.roles.map(el => el.name),
-                },
-                token: token
-            };
-        }
-    },
+        login,
+        // TODO: add signup resolver
+    }
 };

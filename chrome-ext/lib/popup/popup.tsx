decac6fd5c0d629ad6422b03601c5662e074a308
Ezzat Chamudi <echamudi@outlook.com>
2020-10-05 13:22:34 +0300

    Add apollo type

--- chrome-ext/lib/popup/popup.tsx
+++ chrome-ext/lib/popup/popup.tsx
@@ -2,10 +2,10 @@ import React, { SyntheticEvent } from 'react';
 import ReactDOM from 'react-dom';
 import { AppState, AnalysisConfig, AnalysisResult } from 'Shared/types/types';
 import { colorHarmony } from '../evaluator/extension-side/color-harmony';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 import { gql } from '@apollo/client';
 
-const client = new ApolloClient({
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
   cache: new InMemoryCache()
 });

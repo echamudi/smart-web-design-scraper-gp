fe53fe0b9999a3f6152ddb8faa85e95afab6edd5
Ayman Rahmon <ayman_rahmon@hotmail.com>
2020-10-08 02:03:09 +0300

    adding the main image processing server and the initial docker file that it's going to use (docker image)...

---
+++ micro-services/java/ImageProcessingSpring/src/main/java/com/sdws/ImageProcessingSpring/ImageProcessingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sdws.ImageProcessingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessingSpringApplication.class, args);
+	}
+
+}

b7747b3e15d9c12917199b7643e1543d622adab3
Ezzat Chamudi <echamudi@outlook.com>
2020-10-10 00:06:53 +0300

    Use localstorage instead of sessionstorage

--- client/src/app/_services/token-storage.service.ts
+++ client/src/app/_services/token-storage.service.ts
@@ -12,23 +12,24 @@ export class TokenStorageService {
 
   signOut() {
     window.sessionStorage.clear();
+    window.localStorage.removeItem(TOKEN_KEY);
   }
 
   public saveToken(token: string) {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token);
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY, token);
   }
 
   public getToken(): string {
-    return sessionStorage.getItem(TOKEN_KEY);
+    return localStorage.getItem(TOKEN_KEY);
   }
 
   public saveUser(user) {
-    window.sessionStorage.removeItem(USER_KEY);
-    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+    window.localStorage.removeItem(USER_KEY);
+    window.localStorage.setItem(USER_KEY, JSON.stringify(user));
   }
 
   public getUser(): {username: string, roles: string[]} {
-    return JSON.parse(sessionStorage.getItem(USER_KEY));
+    return JSON.parse(localStorage.getItem(USER_KEY));
   }
 }

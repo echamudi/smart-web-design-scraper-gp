25f4a16e7775b6b6deb01db50ec60459f4334eb8
Ayman Rahmon <ayman_rahmon@hotmail.com>
2020-10-13 22:46:05 +0300

    adding the getting most common color code ...

--- micro-services/java/ImageProcessingSpring/src/main/java/com/sdws/ImageProcessingSpring/resources/DensityResource.java
+++ micro-services/java/ImageProcessingSpring/src/main/java/com/sdws/ImageProcessingSpring/resources/DensityResource.java
@@ -1,6 +1,7 @@
 package com.sdws.ImageProcessingSpring.resources;
 
 import com.sdws.ImageProcessingSpring.models.density.DensityCallBody;
+import com.sdws.ImageProcessingSpring.utils.ColorsUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static com.sdws.ImageProcessingSpring.utils.ColorsUtils.getRGBArr;
 import static com.sdws.ImageProcessingSpring.utils.ImageUtils.decodeImage;
@@ -35,7 +34,7 @@ public class DensityResource {
         int height = buffImg.getWidth() ;
         int width = buffImg.getWidth();
         Map colorsCounts = new HashMap();
-
+         // looping through the image...
         for(int i =0 ; i < width  ; i++) {
             for(int j = 0 ; j < height ; j++) {
                 int rgb = buffImg.getRGB(i,j);
@@ -52,11 +51,29 @@ public class DensityResource {
                 colorsCounts.put(rgb, counter) ;
             }
         }
+        // getting the most common color...
+
+
         return Core.VERSION;
     }
 
 
-
+    private int  gettingMostCommonColor(Map map) {
+    List list = new LinkedList<>(map.entrySet());
+    // sorting the map in reverse order ...
+    Collections.sort(list, new Comparator<>() {
+        @Override
+        public int compare(Object o1, Object o2) {
+            return ((Comparable) ((Map.Entry) (o1)).getValue())
+                    .compareTo(((Map.Entry) (o2)).getValue());
+        }
+    });
+    // return the last item in the map (which is the most used element);
+        Map.Entry me = (Map.Entry )list.get(list.size()-1);
+//        int[] rgb = ColorsUtils.getRGBArr((Integer) me.getKey());
+        // return pixel...
+        return (int)me.getKey() ;
+    }
 
 
 

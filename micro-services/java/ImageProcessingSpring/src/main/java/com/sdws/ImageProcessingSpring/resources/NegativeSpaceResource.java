92c50aaebd8c3c59dad58f7fdd1bbc78b145b6f7
Ayman Rahmon <ayman_rahmon@hotmail.com>
2020-10-25 11:00:40 +0300

    fix a problem with the new negative space route

--- micro-services/java/ImageProcessingSpring/src/main/java/com/sdws/ImageProcessingSpring/resources/NegativeSpaceResource.java
+++ micro-services/java/ImageProcessingSpring/src/main/java/com/sdws/ImageProcessingSpring/resources/NegativeSpaceResource.java
@@ -23,11 +23,11 @@ public class NegativeSpaceResource {
     @PostMapping("/test")
     public NegativeSpaceResult checkNegativeSpace(@RequestBody NegativeSpaceCallBody body) {
 
-        return new NegativeSpaceResult(checkNegativeSpace(ImageUtils.decodeImage(body.getImg()))) ;
+        return checkNegativeSpace(ImageUtils.decodeImage(body.getImg()));
     }
 
     public NegativeSpaceResult checkNegativeSpace( String img) {
-
+        System.out.println("negative space : ");
         return new NegativeSpaceResult(checkNegativeSpace(ImageUtils.decodeImage(img))) ;
     }
 
@@ -35,14 +35,22 @@ public class NegativeSpaceResource {
         Map colorsCounts = ImageUtils.getColorCounts(buffImg);
         // getting the most common color...
         int backgroundColor = ColorsUtils.getMostCommonColor(colorsCounts) ;
+//        System.out.println( " most common color : " + backgroundColor);
         int  backgroundPixels = (int)colorsCounts.get(backgroundColor) ;
+//        System.out.println("most common color count : " +backgroundPixels ) ;
 
         //  getting percentage of the dominant color in the rest of the image ...
         int allPixels =  buffImg.getWidth() * buffImg.getHeight() ;
+//        System.out.println("all pixels ::> " + allPixels) ;
         double percentageOfNegativeSpace = ((double)backgroundPixels / (double)allPixels) *100 ;
+        double percentageOfDensity = 100 -  percentageOfNegativeSpace  ;
+//        System.out.println("percentage of Density ::>  "+percentageOfDensity);
+//        System.out.println( "percentage of negative Space : " + percentageOfNegativeSpace) ;
+
 
         //        construct the callback and return it...
         return new NegativeSpaceResult(percentageOfNegativeSpace,allPixels,backgroundColor,backgroundPixels);
+//        return String.valueOf(percentageOfDominantColor);
     }
 
 }

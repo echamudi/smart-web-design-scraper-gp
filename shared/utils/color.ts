7e25c0ecf44e7c68608529b7779c13385d12c4db
Ezzat Chamudi <echamudi@outlook.com>
2020-10-26 21:37:26 +0300

    Add type annotation for color utils

--- shared/utils/color.ts
+++ shared/utils/color.ts
@@ -5,11 +5,11 @@ export function equalWithTolerance(a: number, b: number, tolerance: number): boo
     return val < tolerance;
 }
 
-export function componentToHex(c) {
+export function componentToHex(c: number) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
 }
 
-export function rgbToHex(r, g, b) {
+export function rgbToHex(r: number, g: number, b: number) {
     return componentToHex(r) + componentToHex(g) + componentToHex(b);
 }

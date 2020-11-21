976afbd2e8800dd0e5c79954cf77545fbeef7d07
Ezzat Chamudi <echamudi@outlook.com>
2020-11-21 19:38:35 +0300

    Add plot

---
+++ shared/utils/plot.ts
@@ -0,0 +1,41 @@
+// import { Canvas } from 'canvas';
+
+// /**
+//  * For plotting components on canvas
+//  * @param components 
+//  * @param config 
+//  */
+// export function plotter(
+//     components: Array<{
+//         w: number,
+//         x: number,
+//         y: number,
+//         h: number
+//     }>,
+//     canvas: HTMLCanvasElement | Canvas,
+//     config: {
+//         height: number,
+//         width: number,
+//         blockColor?: string,
+//     }): HTMLCanvasElement | Canvas {
+//     const blockColor: string = config.blockColor ?? '#000000';
+
+//     // const canvas = document.createElement('canvas');
+
+//     canvas.width = config.width;
+//     canvas.height = config.height;
+
+//     const ctx = canvas.getContext('2d');
+
+//     // Fill with white
+//     ctx.fillStyle = '#ffffff'
+//     ctx.fillRect(0, 0, config.width, config.height);
+
+//     // Add blocks
+//     ctx.fillStyle = '#000000';
+//     components.forEach((rect) => {
+//         ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
+//     });
+
+//     return canvas;
+// }

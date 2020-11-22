b56991133ff4b408bed77f1d885171e87f51b014
Ezzat Chamudi <echamudi@outlook.com>
2020-11-22 16:55:41 +0300

    Update plotter

--- shared/utils/canvas.ts
+++ shared/utils/canvas.ts
@@ -1,35 +1,32 @@
-import { Canvas, NodeCanvasRenderingContext2DSettings } from 'canvas';
+import { Canvas } from 'canvas';
 
 /**
  * For plotting components on canvas
  * @param components 
  * @param config 
  */
-export function plotter(
-    components: Array<{
-        w: number,
-        x: number,
-        y: number,
-        h: number
-    }>,
-    canvas: HTMLCanvasElement | Canvas,
-    config: {
-        height: number,
-        width: number,
-        blockColor?: string,
-    }): HTMLCanvasElement | Canvas {
-    const blockColor: string = config.blockColor ?? '#000000';
+function plotter<T extends Canvas | HTMLCanvasElement>(
+    canvas: T,
+    components: Array<{ w: number, x: number, y: number, h: number }>,
+    config: { pageHeight: number, pageWidth: number, tileSize: number, })
+    : {
+        canvas: T,
+        distribution: number[][]
+    } {
 
-    // const canvas = document.createElement('canvas');
+    const { pageHeight, pageWidth, tileSize } = config;
 
-    canvas.width = config.width;
-    canvas.height = config.height;
+    const totalColumns = Math.floor(pageWidth / tileSize);
+    const totalRows = Math.floor(pageHeight / tileSize);
 
-    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
+
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D | ReturnType<Canvas['getContext']>;
 
     // Fill with white
     ctx.fillStyle = '#ffffff';
-    ctx.fillRect(0, 0, config.width, config.height);
+    ctx.fillRect(0, 0, pageWidth, pageHeight);
 
     // Add blocks
     ctx.fillStyle = '#000000';
@@ -37,5 +34,53 @@ export function plotter(
         ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
     });
 
-    return canvas;
+    // Get percentages
+    const distribution: number[][] = Array<number[]>(totalRows);
+
+    for (let row = 0; row < (pageHeight / tileSize); row++) {
+        distribution[row] = Array<number>(totalColumns).fill(-1);
+
+        for (let col = 0; col < (pageWidth / tileSize); col++) {
+            const imageData = ctx.getImageData(col * tileSize, row * tileSize, tileSize, tileSize);
+            const imagePixels = imageData.data;
+
+            let filledPixels = 0;
+
+            for (let i = 0; i < imagePixels.length; i += 4) {
+                if (imagePixels[i] === 0) {
+                    filledPixels += 1;
+                }
+            }
+
+            const ratio = filledPixels / (tileSize * tileSize);
+
+            distribution[row][col] = ratio;
+
+            // Draw number
+            // ctx.font = "bold 20px Arial";
+            // ctx.fillStyle = "#8795ff";
+            // ctx.fillText(ratio + '', col * tileSize + 2, row * tileSize + 22);
+
+            // Draw lines
+            ctx.strokeStyle = 'red';
+            for (let i = 0; i < pageWidth; i += tileSize) {
+                ctx.beginPath();
+                ctx.moveTo(i, 0);
+                ctx.lineTo(i, pageHeight);
+                ctx.stroke();
+            }
+
+            for (let i = 0; i < pageHeight; i += tileSize) {
+                ctx.beginPath();
+                ctx.moveTo(0, i);
+                ctx.lineTo(pageWidth, i);
+                ctx.stroke();
+            }
+        }
+    }
+
+    return {
+        canvas,
+        distribution
+    };
 }

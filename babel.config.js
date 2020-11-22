38493f56111cfa71ad41f25305629b473bc6562a
Ezzat Chamudi <echamudi@outlook.com>
2020-11-22 20:51:08 +0300

    Add jest

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+// For jest
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env', {targets: {node: 'current'}}
+        ],
+        '@babel/preset-typescript'
+    ]
+};
+

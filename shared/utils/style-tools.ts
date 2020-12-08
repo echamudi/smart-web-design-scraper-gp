ff892de2c746b7872cd0a1461fcc54e2b7b99db8
Ezzat Chamudi <echamudi@outlook.com>
2020-12-08 05:40:24 +0300

    Remove style tools

--- shared/utils/style-tools.ts
+++ shared/utils/style-tools.ts
@@ -1,11 +1,11 @@
-export function styleElementFactory(dataLabel: string) {
-    if (document.querySelector(`[data-swds-styleElem-${dataLabel}='1']`) !== null) return;
+// export function styleElementFactory(dataLabel: string) {
+//     if (document.querySelector(`[data-swds-styleElem-${dataLabel}='1']`) !== null) return;
 
-    const elem = document.createElement('style');
-    elem.setAttribute('data-swds-styleElem-' + dataLabel, '1');
-    document.getElementsByTagName("head")[0].appendChild(elem);
-}
+//     const elem = document.createElement('style');
+//     elem.setAttribute('data-swds-styleElem-' + dataLabel, '1');
+//     document.getElementsByTagName("head")[0].appendChild(elem);
+// }
 
-export function getStyleElement(dataLabel: string) {
-    return document.querySelector(`[data-swds-styleElem-${dataLabel}='1']`);
-}
+// export function getStyleElement(dataLabel: string) {
+//     return document.querySelector(`[data-swds-styleElem-${dataLabel}='1']`);
+// }

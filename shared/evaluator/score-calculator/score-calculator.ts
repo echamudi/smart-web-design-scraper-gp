1de9f4c9259e46105324b5af1b0c9b7e2b42353d
Ezzat Chamudi <echamudi@outlook.com>
2020-12-10 19:34:53 +0300

    Rename Component to Element

--- shared/evaluator/score-calculator/score-calculator.ts
+++ shared/evaluator/score-calculator/score-calculator.ts
@@ -1,6 +1,6 @@
 import { FeatureExtractorResult } from 'Shared/types/feature-extractor';
 import { plotter } from 'Shared/utils/canvas';
-import { ComponentPosition } from 'types/types';
+import { ElementPosition } from 'types/types';
 
 /**
  * Final Score Calculator
@@ -12,18 +12,18 @@ export function scoreCalculator(doc: Document, features: FeatureExtractorResult)
     const plotterConfig = { pageHeight, pageWidth, tileSize };
 
     const textPlotCanvas: HTMLCanvasElement = doc.createElement('canvas');
-    const textComponents: ComponentPosition[] = features.textDetection.components.map((el) => el.position);
+    const textComponents: ElementPosition[] = features.textElements.elements.map((el) => el.position);
     const textPlot = plotter(textPlotCanvas, textComponents, plotterConfig);
 
     const imagePlotCanvas: HTMLCanvasElement = doc.createElement('canvas');
-    const imageComponents: ComponentPosition[] = features.imageDetection.components.map((el) => el.position);
+    const imageComponents: ElementPosition[] = features.imageElements.elements.map((el) => el.position);
     const imagePlot = plotter(imagePlotCanvas, imageComponents, plotterConfig);
 
     const videoPlotCanvas: HTMLCanvasElement = doc.createElement('canvas');
-    const videoComponents: ComponentPosition[] = features.videoDetection.components.map((el) => el.position);
+    const videoComponents: ElementPosition[] = features.videoElements.elements.map((el) => el.position);
     const videoPlot = plotter(videoPlotCanvas, videoComponents, plotterConfig);
 
     const anchorPlotCanvas: HTMLCanvasElement = doc.createElement('canvas');
-    const anchorComponents: ComponentPosition[] = features.anchorDetection.components.map((el) => el.position);
+    const anchorComponents: ElementPosition[] = features.anchorElements.elements.map((el) => el.position);
     const anchorPlot = plotter(anchorPlotCanvas, anchorComponents, plotterConfig);
 }

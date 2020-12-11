69f8d2e95d8939f79329dcd690962f2963cfb71c
Ezzat Chamudi <echamudi@outlook.com>
2020-12-11 19:35:42 +0300

    Add module resolver for jest

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,29 @@
+const path = require('path');
+
 // For jest
 module.exports = {
     presets: [
         [
-            '@babel/preset-env', {targets: {node: 'current'}}
+            '@babel/preset-env', {
+                targets: {
+                    node: 'current'
+                }
+            }
         ],
         '@babel/preset-typescript'
-    ]
+    ],
+    plugins: [
+        ["module-resolver", {
+            root: ["./"],
+            alias: {
+                "Shared": './shared',
+                "ChromeExt": './chrome-ext/lib',
+                "Angular": './client/src',
+                "Express": './server/src',
+            }
+        }]
+    ],
+    parserOpts: {
+        strictMode: true
+    }
 };
-

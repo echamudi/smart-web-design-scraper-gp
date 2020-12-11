db8b63cc15ac653e3b354634ffeb6a61a35bf971
Ezzat Chamudi <echamudi@outlook.com>
2020-12-11 20:53:04 +0300

    Fix algorithm

--- shared/evaluator/score-calculator/final.ts
+++ shared/evaluator/score-calculator/final.ts
@@ -7,26 +7,27 @@ import { blockDensityScoreCalculate } from './block-density';
  * Final Score Calculator
  */
 export function finalScoreCalculate(doc: Document, features: FeatureExtractorResult) {
-    const tileSize = Math.floor(features.browserInfo.viewportWidth);
+    const tileSize = Math.floor(features.browserInfo.viewportWidth / 4);
     const pageHeight = features.browserInfo.scrollHeight;
     const pageWidth = features.browserInfo.scrollWidth;
     const plotterConfig = { pageHeight, pageWidth, tileSize };
 
     const textPlotCanvas: HTMLCanvasElement = doc.createElement('canvas');
-    const textElements: ElementPosition[] = features.textElements.elements.map((el) => el.position);
+    const textElements: ElementPosition[] = [];
+    features.textElements.elements.forEach((el) => {
+        if (el.visible) textElements.push(el.position);
+    });
     const { distribution: textElementDistribution } = plotter(textPlotCanvas, textElements, plotterConfig);
 
     const imagePlotCanvas: HTMLCanvasElement = doc.createElement('canvas');
-    const imageElements: ElementPosition[] = features.imageElements.elements.map((el) => el.position);
+    const imageElements: ElementPosition[] = [];
+    features.imageElements.elements.forEach((el) => {
+        if (el.visible) imageElements.push(el.position);
+    });
     const { distribution: imageElementDistribution }  = plotter(imagePlotCanvas, imageElements, plotterConfig);
 
-    // const videoPlotCanvas: HTMLCanvasElement = doc.createElement('canvas');
-    // const videoElements: ElementPosition[] = features.videoElements.elements.map((el) => el.position);
-    // const { distribution: videoElementDistribution } = plotter(videoPlotCanvas, videoElements, plotterConfig);
-
-    // const anchorPlotCanvas: HTMLCanvasElement = doc.createElement('canvas');
-    // const anchorElements: ElementPosition[] = features.anchorElements.elements.map((el) => el.position);
-    // const { distribution: anchorElementDistribution } = plotter(anchorPlotCanvas, anchorElements, plotterConfig);
+    // document.body.appendChild(textPlotCanvas);
+    // document.body.appendChild(imagePlotCanvas);
 
     return {
         textDensity: blockDensityScoreCalculate(textElementDistribution),

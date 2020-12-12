778e42d30814cafaafd6a313cba4070f58f0a338
Ezzat Chamudi <echamudi@outlook.com>
2020-12-12 08:02:58 +0300

    Add cohesion

---
+++ shared/evaluator/score-calculator/cohesion.test.ts
@@ -0,0 +1,54 @@
+import { cohesionScoreCalculate } from './cohesion';
+
+test('standard', () => {
+    expect(
+        cohesionScoreCalculate([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    ).toStrictEqual(
+        {
+            data: {
+                totalMembers: 9,
+                members: [
+                    1, 2, 3, 4, 5,
+                    6, 7, 8, 9
+                ],
+                totalUniqueMembers: 9,
+                uniqueMembers: [
+                    1, 2, 3, 4, 5,
+                    6, 7, 8, 9
+                ]
+            },
+            score: 0.11111111111111116
+        }
+    );
+
+    expect(
+        cohesionScoreCalculate([1.64, 1.64, 1.63, 1.62, 2.12, 2.13])
+    ).toStrictEqual(
+        {
+            data: {
+                totalMembers: 6,
+                members: [1.64, 1.64, 1.63, 1.62, 2.12, 2.13],
+                totalUniqueMembers: 2,
+                uniqueMembers: [1.6, 2.1]
+            },
+            score: 0.8888888888888888
+        }
+    );
+
+    expect(
+        cohesionScoreCalculate([5,5,5,5,5,5,5,5,5])
+    ).toStrictEqual(
+        {
+            data: {
+                totalMembers: 9,
+                members: [
+                    5, 5, 5, 5, 5,
+                    5, 5, 5, 5
+                ],
+                totalUniqueMembers: 1,
+                uniqueMembers: [5]
+            },
+            score: 1
+        }
+    );
+});
---
+++ shared/evaluator/score-calculator/cohesion.ts
@@ -0,0 +1,40 @@
+import { scaleValue } from "Shared/utils/math";
+
+export interface CohesionScoreCalculateResult {
+    data: {
+        totalMembers: number | undefined,
+        members: number[] | undefined;
+        /**
+         * Total members without duplicate
+         */
+        totalUniqueMembers: number | undefined,
+        uniqueMembers: number[] | undefined;
+    },
+    score: number | undefined
+}
+
+export function cohesionScoreCalculate(members: number[]): CohesionScoreCalculateResult {
+    if (members.length === 0) return {
+        data: {
+            totalMembers: undefined,
+            members: undefined,
+            totalUniqueMembers: undefined,
+            uniqueMembers: undefined
+        },
+        score: undefined
+    };
+
+    // Rounding to 1 decimal place
+    const roundedNumbers = members.map((val) => Math.round(val * 10) / 10);
+    const totalMembers = roundedNumbers.length;
+    const uniqueMembers = [...new Set(roundedNumbers)];
+    const totalUniqueMembers = uniqueMembers.length;
+
+    const scaledTotalUniqueMembers = scaleValue(totalUniqueMembers, [1, 10], [0, 1]);
+    const score = 1 - scaledTotalUniqueMembers;
+
+    return {
+        data: { totalMembers, members, totalUniqueMembers, uniqueMembers},
+        score
+    }
+}
